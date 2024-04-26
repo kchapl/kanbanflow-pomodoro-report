5d4fcda551804ca9ae893321601f87c364eb166b
kchapl <kchapl@users.noreply.github.com>
2024-04-26 18:21:14 +0000

    Update circe-parser to 0.14.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val requests = "com.lihaoyi" %% "requests" % "0.8.2"
-  lazy val circe = "io.circe" %% "circe-parser" % "0.14.6"
+  lazy val circe = "io.circe" %% "circe-parser" % "0.14.7"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
 }
