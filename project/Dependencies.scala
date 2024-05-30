3876e20083956525a3cd0d7208516f515cd66e99
kchapl <kchapl@users.noreply.github.com>
2024-05-30 18:25:26 +0000

    Update requests to 0.8.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val requests = "com.lihaoyi" %% "requests" % "0.8.2"
+  lazy val requests = "com.lihaoyi" %% "requests" % "0.8.3"
   lazy val circe = "io.circe" %% "circe-parser" % "0.14.7"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
 }
