950f225f532ff7a221dd77fd2fabda8a0de4a833
kchapl <kchapl@users.noreply.github.com>
2024-07-24 20:42:26 +0000

    Update scalatest to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val requests = "com.lihaoyi" %% "requests" % "0.9.0"
   lazy val circe = "io.circe" %% "circe-parser" % "0.14.9"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
 }
